471fee291ebd720568aa7a646248dbc350ef4441
superwenzg <superwenzg@gmail.com>
2017-05-10 13:01:15 +0800

    bug fixed

--- interpolation/Newdon polynomial/Newdon polynomial.cc
+++ interpolation/Newdon polynomial/Newdon polynomial.cc
@@ -1,6 +1,6 @@
 /*
-    used to caculate the sin fuc
-    @   amaswz superwenzg@gmail.com
+	used to caculate the sin fuc
+	@	amaswz superwenzg@gmail.com
  */
 
 #include <cstdio>
@@ -10,89 +10,126 @@
 #include <fstream>
 #include <iostream>
 
-const long double RIGHT=0.3367;
-const long double LEFT=0.000;
-const long double SIGMA=0.0001;
 
 using namespace std;
+typedef pair <double,double> Point;
 
-void init(int n,double **Part_x,double **Part_y,double &Left,double &Right,double &Sigma)
+class Newdon_it
 {
-    Left=LEFT;
-    Right=RIGHT;
-    Sigma=SIGMA;
-    *Part_x=new double[n];
-    *Part_y=new double[n];
-    `
-    cout<<"input the interpolation argv"<<endl;
-    for(int i=0;i<n;i++)
+public:
+    Newdon_it(const int num_of_inter_point)
+    :_noip(num_of_inter_point)
     {
-        (*Part_x)[i]=0;
-        cin>>(*Part_x)[i];
-        (*Part_y)[i]=0;
-        cin>>(*Part_y)[i];
-    }
-}
-
-double caculate(const double *Part_x,const double *Part_y,double *List,int jie,
-                const int Left,const int Right)
-{
-    if(Left==Right)
-    {
+        _array = new Point[_noip];
+        _ans = new double[_noip];
         
-        return Part_y[Left];
-    }
-    else
-    {
-        double a,b,s;
-        a=caculate(Part_x,Part_y,List,jie-1,Left+1,Right);
-        b=caculate(Part_x,Part_y,List,jie-1,Left,Right-1);
-        s= (a-b)/(Part_x[Right]-Part_x[Left]);
-        
-        if(Left==0)
+        for(int i=0;i<_noip;i++)
         {
-            List[jie]=s;
+            cin>>_array[i].first>>_array[i].second;
+            _ans[i] = _array[i].second;
         }
-        return s;
+    };
+    
+    void set_order(int order)
+    {
+        _order = order;
+    }
+    
+    void res(double xt);
+    void calcaute();
+private:
+    int _noip; //插值点个数
+    int _order;
+    Point *_array; //插值点数组
+    
+    double *_ans; //牛顿插值表
+};
+
+void Newdon_it::calcaute()
+{
+    for(int i=0;i<_noip;i++)
+    {
+        for(int j=_noip-1;j>i;j--)
+        {
+            _ans[j] = (_ans[j] - _ans[j-1] ) /
+            ( _array[j].first - _array[j-1-i].first);
+        }
+        
     }
 }
 
+void Newdon_it::res(double xt)
+{
+    double Res = 0;
+    double iteration_part = 1;
+    
+    Res = _ans[0];
+    
+    for(int i=1;i<_order;i++)
+    {
+        iteration_part *= (xt - _array[i-1].first);
+        Res += _ans[i] * iteration_part;
+    }
+    
+    cout<<Res<<endl;
+}
+
+void caculate(const double *Part_x,const double *Part_y,double *List,
+              const int Left,const int Right)
+{
+    double Ans[10];
+    memset(Ans,0,sizeof Ans);
+    for(int i=0;i<6;i++)
+    {
+        Ans[i]=Part_y[i];
+    }
+    
+    for(int i=1;i<Right;i++)
+    {
+        for(int j=Right-1;j>=i;j--)
+        {
+            Ans[j]=(Ans[j]-Ans[j-1])/(Part_x[j]-Part_x[j-i]);
+        }
+    }
+    for(int i=0;i<Right;i++)
+    {
+        cout<<Ans[i]<<endl;
+    }
+}
 
 int main(int argc,char** argv)
 {
-    if(argc<=1)
-    {
-        cout<<"Error argv input"<<endl;
-        exit(0);
-    }
+    int num_of_inter_point; //插值点的个数
+    int Order; //阶数
+    double xt; //欲计算的点
     
-    int Num_of_interpolation;
-    int Num_of_argv;
-    int cnt;
+    cout<<"输入插值点个数: ";
+    cin>>num_of_inter_point;
     
-    double Left;
-    double Right;
-    double sigma;
-    double *Part_x;
-    double *Part_y;
-    double list[100];
+    Newdon_it Ni(num_of_inter_point);
+    Ni.calcaute();
     
-    //transf the string to num
-    stringstream s;
-    s<<argv[1];
-    s>>Num_of_interpolation;
-    cout<<Num_of_interpolation;
-    //s>>Num_of_argv;
-    cnt=Num_of_argv;
+    cout << "输入欲计算阶数: ";
+    cin >> Order;
+    Ni.set_order(Order);
     
-    
-    
-    init(Num_of_interpolation,&Part_x,&Part_y,Left,Right,sigma);
-    caculate(Part_x,Part_y,list,5,0,5);
-    
-    list[0]=Part_y[0];
-    for(int i=0;i<5;i++)
-    {
-        cout<<list[i]<<endl;
-    }
+    cout << "输入欲计算点xt(control + c退出运算): ";
+    while( cin>>xt ) Ni.res(xt);
 }
+
+/*
+ 0.40 0.41075
+ 0.55 0.57815
+ 0.65 0.69675
+ 0.80 0.88811
+ 0.90 1.02652
+ 1.05 1.25382
+ 
+ 0.017037 1.017183
+ 0.146447 1.157713
+ 0.370590 1.448590
+ 0.629410 1.876502
+ 0.853553 2.347975
+ 0.982963 2.672363
+ */
+
